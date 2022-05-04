084061b84923a19946643fb651a052a0adb371c4
wintermooon <mooon8630@gmail.com>
2022-05-04 14:14:24 +0900

    [programmers]: 크레인 인형 뽑기

---
+++ pro_크레인 인형 뽑기.py
@@ -0,0 +1,25 @@
+def solution(board, moves):
+    answer = 0
+    basket = []
+    newboard = [[] for _ in range(len(board))]
+    for i in range(len(board)):
+        for j in range(len(board)-1,-1, -1):
+            if board[j][i] != 0:
+                newboard[i].append(board[j][i])
+
+    for i in moves:
+        if len(newboard[i-1]) == 0:
+            continue
+        basket.append(newboard[i-1].pop())
+        if len(basket) < 2:
+            continue
+        if basket[-1] == basket[-2]:
+            basket.pop()
+            basket.pop()
+            answer += 2
+        print(basket) 
+    return answer
+
+board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
+moves = [1,5,3,5,1,2,1,4]
+print(solution(board, moves))
